ed089aceab6f8ddfca92e347abda4b0b92768483
mileschen <417273365@qq.com>
2026-04-21 09:49:06 +0800

    feature: 修复@builder构造函数

--- project/src/main/java/com/nageoffer/shortlink/project/dao/entity/ShortlinkDO.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/entity/ShortlinkDO.java
@@ -3,14 +3,18 @@ package com.nageoffer.shortlink.project.dao.entity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.nageoffer.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 
-@Builder
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @TableName("t_link")
 public class ShortlinkDO extends BaseDO {
     /**
